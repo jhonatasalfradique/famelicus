93a2fffeeb125223a797233b27b41fab7709eac1
guigs3000 <guilhermealmeida3000@gmail.com>
2015-07-09 16:28:19 -0300

    pega localização a cada 10 segs
    ta tudo no mainActivity

--- Famelicus/app/src/main/java/br/ufrj/cos/famelicus/MainActivity.java
+++ Famelicus/app/src/main/java/br/ufrj/cos/famelicus/MainActivity.java
@@ -1,39 +1,60 @@
 package br.ufrj.cos.famelicus;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.location.LocationManager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+        import android.app.Activity;
+        import android.app.PendingIntent;
+        import android.content.BroadcastReceiver;
+        import android.content.Intent;
+        import android.content.IntentSender;
+        import android.location.Location;
+        import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
+
+        import com.google.android.gms.common.ConnectionResult;
+        import com.google.android.gms.common.GooglePlayServicesUtil;
+        import com.google.android.gms.common.api.GoogleApiClient;
+        import com.google.android.gms.common.api.Status;
+        import com.google.android.gms.location.Geofence;
+        import com.google.android.gms.location.LocationListener;
+        import com.google.android.gms.location.LocationRequest;
+        import com.google.android.gms.location.LocationServices;
+        import com.google.android.gms.common.api.PendingResult;
+
+        import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity
+        implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static int REQUEST_CODE_RECOVER_PLAY_SERVICES = 200;
+
+    private GoogleApiClient mClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
 
     Button situacaoFila;
     Button colaborar;
     Aplicativo famelicus;
-//    Servico servico;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        famelicus = new Aplicativo(this);
-        //famelicus.LigarGPS();
-
-        //Servico servico = new Servico(famelicus);
-        //servico.ChamarAplicativo();
+        createLocationRequest();
+        // you can also add more APIs and scopes here
+        mClient = new GoogleApiClient.Builder(this, this, this)
+                .addApi(LocationServices.API).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
         situacaoFila = (Button) findViewById(R.id.situacaoFila);
         colaborar = (Button) findViewById(R.id.colaborar);
-
+        famelicus = new Aplicativo();
 
         situacaoFila.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,32 +75,84 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        mClient.connect();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStop() {
+        mClient.disconnect();
+        super.onStop();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // this callback will be invoked when all specified services are connected
+        startUpdates();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // this callback is invoked when location updates
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // this callback will be invoked when the client is disconnected
+        // it might happen e.g. when Google Play service crashes
+        // when this happens, all requests are canceled,
+        // and you must wait for it to be connected again
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // this callback will be invoked when the connection attempt fails
+
+        if (connectionResult.hasResolution()) {
+            // Google Play services can fix the issue
+            // e.g. the user needs to enable it, updates to latest version
+            // or the user needs to grant permissions to it
+            try {
+                connectionResult.startResolutionForResult(this, 0);
+            } catch (IntentSender.SendIntentException e) {
+                // it happens if the resolution intent has been canceled,
+                // or is no longer able to execute the request
+            }
+        } else {
+            // Google Play services has no idea how to fix the issue
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000*10);
+        mLocationRequest.setFastestInterval(1000*5);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
-
+    protected void startUpdates(){
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mClient, mLocationRequest, this);
+    }
+    protected void stopUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mClient, this);
+    }
+//    protected void startBackgroundUpdates(){
+//                LocationServices.FusedLocationApi.requestLocationUpdates(
+//                mClient, mLocationRequest, mPendingIntent);
+//    }
+//    protected void stopBackgroundUpdates(){
+//        LocationServices.FusedLocationApi. removeLocationUpdates(
+//                mClient, mPendingIntent);
+//    }
 }
